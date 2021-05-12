016f9ad888c1db16ef85625d69fc89bd40db037e
mikej <michael.jackson@digital.hmrc.gov.uk>
2021-05-12 09:18:27 +0100

    Upgrade language toggle

---
+++ it/helpers/ViewSpec.scala
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package helpers
+
+import org.jsoup.nodes.Element
+import org.scalatest.Matchers.fail
+import play.twirl.api.TwirlHelperImports.twirlJavaCollectionToScala
+
+trait ViewSpec {
+
+  implicit class CustomSelectors(element: Element) {
+
+    def selectFirst(selector: String): Element = {
+      element.select(selector).headOption match {
+        case Some(element) => element
+        case None => fail(s"No elements returned for selector: $selector")
+      }
+    }
+
+    def selectOptionally(selector: String): Option[Element] = {
+      element.select(selector).headOption
+    }
+  }
+}
