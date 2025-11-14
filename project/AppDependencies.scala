c5d387f7086ecbe2cd92c73a0bb897904c0df8d6
bkarimii <125777824+bkarimii@users.noreply.github.com>
2025-11-14 11:50:38 +0000

    ITSASU-4552

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
   val appName = "income-tax-subscription-self-employed-frontend"
 
   private val bootstrapVersion = "10.4.0"
-  private val playHmrcFrontendVersion = "12.6.0"
+  private val playHmrcFrontendVersion = "12.20.0"
   private val catsVersion = "2.13.0"
   private val cryptoJsonVersion = "8.4.0"
 
